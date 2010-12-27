a02d692e5c85b54da7c8821452cc6aa503a15eb4
Randy Stauner <randy@magnificent-tears.com>
2010-12-27 10:18:33 -0700

    Add on_undef Enum and put both enums in our %Enums

--- lib/Data/Transform/Named/Stackable.pm
+++ lib/Data/Transform/Named/Stackable.pm
@@ -23,8 +23,12 @@ use Data::Transform::Named;
 use Object::Enum 0.072 ();
 use Set::DynamicGroups ();
 
-our $WarnNoField = Object::Enum->new({unset => 0, default => 'single',
-	values => [qw(never single always)]});
+our %Enums = (
+	on_undef => Object::Enum->new({unset => 0, default => 'skip',
+		values => [qw(skip blank proceed)]}),
+	warn_no_field => Object::Enum->new({unset => 0, default => 'single',
+		values => [qw(never single always)]}),
+);
 
 =method new
 
@@ -51,6 +55,23 @@ Possible options:
 An instance of Data::Transform::Named.
 A default (with all the functions from L<Data::Transform::Named::Common>)
 will be created if not supplied.
+
+* I<on_undef>
+What to do when a value is undefined.
+Valid values are:
+
+=begin :list
+
+* C<proceed> - proceed as normal (as if it was defined)
+
+* C<skip> - skip the transformation (don't call the sub)
+
+* C<blank> - initialize the value to a blank string
+
+=end :list
+
+The default is C<skip> since must functions likely expect
+some sort of value (like a string) to transform.
 
 * I<warn_no_field>
 Whether or not to emit a warning if asked to transform a field
@@ -87,8 +108,11 @@ sub new {
 		fields => {},
 		groups => Set::DynamicGroups->new(),
 		queue  => [],
-		warn_no_field =>
-			$WarnNoField->clone(delete $opts{warn_no_field} || 'single'),
+	};
+	while( my ($name, $enum) = each %Enums ){
+		$self->{$name} = $enum->clone(
+			exists $opts{$name} ? delete $opts{$name} : ()
+		);
 	};
 
 	bless $self, $class;
